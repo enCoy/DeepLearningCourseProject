5cf176725d3b14b8107fde708757dc092c151249
enCoy <c.okanyaldiz@gmail.com>
2022-10-31 14:24:55 -0400

    Standardization is added to the pipeline.

--- dataset/dataloader.py
+++ dataset/dataloader.py
@@ -8,6 +8,7 @@ import cv2
 from utils.utils import load_coord, load_colored, load_mask, load_label, load_depth
 import os
 import random
+import time
 
 class CustomDataLoader(Dataset):
     # returns
@@ -16,6 +17,7 @@ class CustomDataLoader(Dataset):
         self.data_directory = data_directory
         self.data_list = os.listdir(data_directory)
         self.sampling_size = sampling_size
+        self.feature_statistics = None
         random.shuffle(self.data_list)
 
     def __len__(self):
@@ -40,18 +42,52 @@ class CustomDataLoader(Dataset):
         # concatenate data column wise
         features = np.concatenate((x_depth, y_depth, z_depth, rgb_data), axis=1).astype(np.float32)
         labels = file['bbox_3d'].astype(np.float32)
-        # these will need normalization, currently no normalization is applied
+        # convert to tensor
+        features = torch.tensor(features)
+        labels = torch.tensor(labels)
+        if self.feature_statistics is not None:  # if we calculated feature statistics and assigned it
+            features -= self.feature_statistics[0]  # mean subtraction
+            features /= self.feature_statistics[1]  # std division
 
         # features shape (self.sampling_size, num_features=6)
         # labels shape (3, 8) - 8 from total num of vertices, 3 from xyz
-        return torch.tensor(features), torch.tensor(labels)
+        return features, labels
+
+    def apply_feature_stat(self, feature_stat):
+        self.feature_statistics = feature_stat
+
+def calculate_feature_statistics(data_loader, feature_dim = 6):
+    general_sum = torch.zeros(feature_dim)
+    general_std_sum = torch.zeros(feature_dim)
+    counter = 0
+    num_batches = len(data_loader)
+
+    starting_time = time.time()
+    for x, y in data_loader:
+        # x is a tensor shaped: (batch_size ,sampling_size=900, num_feature=6)
+        # y is a tensor shaped: (batch_size ,3, 8)
+        # find the average x,y,z in the batch
+        average_sample_coord = torch.mean(x, 1) #  batch_size x num_feature
+        # sum over batch
+        general_sum += average_sample_coord.sum(axis=0)
+        general_std_sum += torch.std(average_sample_coord, 0)  # std of average sample coord across the batch
+        # counter holds the total number of examples
+        counter += average_sample_coord.size()[0]
+    average_batch_coord = general_sum / counter
+    std_batch_coord = general_std_sum / num_batches
+    finishing_time = time.time()
+    print(
+        f"One pass on training data for calculating feature statistics took {finishing_time - starting_time} seconds.")
+    return average_batch_coord, std_batch_coord
 
 if __name__ == "__main__":
     data_directory = r'C:\Users\Cem Okan\Dropbox (GaTech)\deep_learning_data\gts\train_data'
     dataset = CustomDataLoader(data_directory, sampling_size=900)
-    data_loader = DataLoader(dataset, batch_size=16, shuffle=True)
+    data_loader = DataLoader(dataset, batch_size=128, shuffle=True)
+    mean_stat, std_stat = calculate_feature_statistics(data_loader, feature_dim=6)
+    print("Feature mean statistics:", mean_stat)
+    print("Feature std statistics:", std_stat)
+    # apply feature statistics to data loader
+    dataset.apply_feature_stat([mean_stat, std_stat])
     for x, y in data_loader:
-        # x is a tensor shaped: (batch_size ,sampling_size=900, num_feature=6)
-        # y is a tensor shaped: (batch_size ,3, 8)
-        print("here is x: ", x.size())
-        print("here is y: ", y.size())
+        continue
